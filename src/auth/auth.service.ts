125cf209d4ea7e9500b654347f9119eab761217d
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-10-12 13:41:38 -0300

    created authentications routes

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Users } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validadeUser(email: string, password: string) {
+    let user: Users;
+    try {
+      user = await this.userService.findOneOrFail({ email });
+    } catch (error) {
+      return null;
+    }
+
+    const isPasswordValid = compareSync(password, user.password);
+    if (!isPasswordValid) return null;
+
+    return user;
+  }
+
+  async login(user: Users) {
+    const payload = { sub: user.id, email: user.email };
+    return {
+      token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    };
+  }
+}
