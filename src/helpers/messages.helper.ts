71ce91bd923c9b1752aa00a6e43b004b8b7d1b51
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-10-12 15:57:22 -0300

    privatizando roda de usuario

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -1,3 +1,5 @@
 export const MessageHelper = {
   PASSWORD_OR_EMAIL_INVALID: 'e-mail or password is invalid',
+  PERMISSION_UNAUTHORIZED:
+    "You don't have permission to access this information",
 };
